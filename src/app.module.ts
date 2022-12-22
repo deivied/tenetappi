d206ed937382c2446f2f28da54c4563660244519
Daouda Diouf <daouda.diouf@ldb.sn>
2022-12-22 12:13:19 +0000

    packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { MongoServiceModule } from './frameworks/persistences/mongo/mongo-service.module';
 import { Inject, Module, UseFilters } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    MongoServiceModule,
     // MongooseModule.forRootAsync({
     //   useFactory: (configService: ConfigService) => ({
     //     uri: configService.get('DB_CLUSTER'),
--- src/use-cases/user/user-factory.service.ts
+++ src/use-cases/user/user-factory.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/core';
 import { CreateUserDto, updateUserDto } from 'src/core/dto/user';
-import { user } from 'src/core/entities/user';
 
 @Injectable()
 export class AuthorFactoryService {
   createNewAuthor(createUserDto: CreateUserDto) {
-    const newUser = new user();
+    const newUser = new User();
     newUser.firstName = createUserDto.firstName;
     newUser.lastName = createUserDto.lastName;
     newUser.email = createUserDto.email;
     newUser.birthDate = createUserDto.birthDate;
 
-    return user;
+    return newUser;
   }
 
   updateAuthor(updateUserDto: updateUserDto) {
-    const updateUser = new user();
+    const updateUser = new User();
     updateUser.firstName = updateUserDto.firstName;
     updateUser.lastName = updateUserDto.lastName;
     updateUser.email = updateUserDto.email;
     updateUser.birthDate = updateUserDto.birthDate;
 
-    return user;
+    return updateUser;
   }
 }
